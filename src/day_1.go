073157d1a360dbf75c353ab5bdf5b7cf7d671311
cberletch <65048267+cberletch@users.noreply.github.com>
2024-12-04 10:55:33 -0500

    Create day_1.go

---
+++ src/day_1.go
@@ -0,0 +1,28 @@
+// src/day_1.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("inputs/day_1.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+}
